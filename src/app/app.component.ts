5e80f97238945963b3c7af37c072aa38305134be
Oleh Chepak <o.chepak@outlook.com>
2018-10-15 22:58:24 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from './users.service';
+import {arrayify} from 'tslint/lib/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-request-task-angular';
+export class AppComponent implements OnInit {
+  title = 'Task 1 | InventorAcademy';
+  users = [];
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit() {
+    this.usersService
+      .getUsers()
+      .subscribe((response) => {
+        this.users = arrayify(response);
+      });
+  }
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class UsersService {
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers() {
+    return this.httpClient.get('https://api.github.com/users');
+  }
+}
